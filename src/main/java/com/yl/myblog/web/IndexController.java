04187ec71d9ab7db78000bcf78b62af896ffa24f
a1978512164 <1978512164@qq.com>
2021-09-14 15:08:49 +0800

    日志处理配置完成

--- src/main/java/com/yl/myblog/web/IndexController.java
+++ src/main/java/com/yl/myblog/web/IndexController.java
@@ -3,6 +3,7 @@ package com.yl.myblog.web;
 import com.yl.myblog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @Author YeLei
@@ -13,13 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
 //        int i = 9/0;
-        String blog = null;
-        if (blog == null){
-            throw  new NotFoundException("博客不存在！");
-        }
+//        String blog = null;
+//        if (blog == null){
+//            throw  new NotFoundException("博客不存在！");
+//        }
+
+        System.out.println("-------index-------");
         return "index";
     }
 }
